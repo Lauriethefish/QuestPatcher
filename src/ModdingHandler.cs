8abcf3372ec4e32570727cc6b14413b3cbbc0b1a
Lauriethefish <lauriethefish@outlook.com>
2021-02-24 21:04:45 +0000

    Moved log

--- src/ModdingHandler.cs
+++ src/ModdingHandler.cs
@@ -96,11 +96,11 @@ namespace MonqueModder
 
         public async Task startModdingProcess()
         {
-            window.log("Creating temporary directory . . .");
             if(Directory.Exists(TEMP_DIRECTORY)) {
                 window.log("Removing existing temporary directory . . .");
                 Directory.Delete(TEMP_DIRECTORY, true); // Delete the temporary directory, since stuff from last time we modded will get in the way
             }
+            window.log("Creating temporary directory . . .");
             Directory.CreateDirectory(TEMP_DIRECTORY);
 
             await downloadFiles();
