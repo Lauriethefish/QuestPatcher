a24d93d6ef1b06b2b826283a3cc924121f894529
Lauriethefish <lauriethefish@outlook.com>
2021-02-26 07:46:17 +0000

    Extra manifest options needed for dependencies

--- src/ModManifest.cs
+++ src/ModManifest.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Text.Json;
 
 namespace QuestPatcher {
+    public class DependencyInfo {
+        public string Id { get; set; }
+
+        public string Version { get; set; }
+
+        public string? DownloadPath { get; set; } = null;
+    }
+
     public class ModManifest {
         public string _QPVersion { get; set; }
         public string Name { get; set; }
@@ -17,15 +25,19 @@ namespace QuestPatcher {
 
         public string Type { get; set; }
 
+        public bool AllowMultipleInstalls { get; set; } = false; // If true, multiple different versions of this mod ID can be installed.
+
         public List<string> ModFiles { get; set; } = new List<string>();
         public List<string> LibraryFiles { get; set; } = new List<string>();
+
+        public List<DependencyInfo> Dependencies { get; set; } = new List<DependencyInfo>();
 
         public static ModManifest Load(string str) {
             JsonSerializerOptions options = new JsonSerializerOptions {
                 PropertyNameCaseInsensitive = true
             };
 
-            return JsonSerializer.Deserialize<ModManifest>(str);
+            return JsonSerializer.Deserialize<ModManifest>(str, options);
         }
     }
 }
