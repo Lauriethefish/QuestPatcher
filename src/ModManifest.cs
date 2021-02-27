872151878b9f6869bf52f4c46a47d71b29c7549f
James Harvey <60884353+James-261@users.noreply.github.com>
2021-02-27 14:30:06 +0000

    Quatcher

--- src/ModManifest.cs
+++ src/ModManifest.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.Json;
 
-namespace QuestPatcher {
+namespace Quatcher {
     public class DependencyInfo {
         public string Id { get; set; }
 
