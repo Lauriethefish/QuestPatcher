0f056c059b49694124d2ec920942e2e93969c1b0
Lauriethefish <lauriethefish@outlook.com>
2021-06-05 20:48:45 +0100

    Fix DMG mount error

--- buildDMG.sh
+++ buildDMG.sh
@@ -1,15 +1,15 @@
-#rm -f macos-dmg/QuestPatcher.dmg
-#echo Building with .NET core . . .
-#dotnet publish -c Release -r osx-x64
-#echo Copying built files to app directory . . .
-#mkdir -p macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS
-#cp -r QuestPatcher/bin/Release/net5.0/osx-x64/publish/* macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/
-#echo Building DMG file . . .
-#hdiutil create -srcfolder macos-dmg/dmg-root /tmp/QuestPatcher.dmg -volname QuestPatcher
-#echo Removing built files . . .
-#rm -r macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/*
-#echo Compressing DMG file . . .
-#hdiutil convert /tmp/QuestPatcher.dmg -format UDZO -o macos-dmg/QuestPatcher.dmg
-#rm /tmp/QuestPatcher.dmg
-#echo Done!
-hdiutil info
+rm -f macos-dmg/QuestPatcher.dmg
+echo Building with .NET core . . .
+dotnet publish -c Release -r osx-x64
+echo Copying built files to app directory . . .
+mkdir -p macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS
+cp -r QuestPatcher/bin/Release/net5.0/osx-x64/publish/* macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/
+echo Building DMG file . . .
+hdiutil info | grep /dev/disk | grep partition | cut -f 1 | xargs hdiutil detach # Fixes attaching error
+hdiutil create -srcfolder macos-dmg/dmg-root /tmp/QuestPatcher.dmg -volname QuestPatcher
+echo Removing built files . . .
+rm -r macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/*
+echo Compressing DMG file . . .
+hdiutil convert /tmp/QuestPatcher.dmg -format UDZO -o macos-dmg/QuestPatcher.dmg
+rm /tmp/QuestPatcher.dmg
+echo Done!
