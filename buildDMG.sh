c5a1207f32dfba169073cb4140822f8d30322839
Lauriethefish <lauriethefish@outlook.com>
2021-06-05 19:40:34 +0100

    Create MacOS directory

--- buildDMG.sh
+++ buildDMG.sh
@@ -2,6 +2,7 @@ rm -f macos-dmg/QuestPatcher.dmg
 echo Building with .NET core . . .
 dotnet publish -c Release -r osx-x64
 echo Copying built files to app directory . . .
+mkdir -p macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS
 cp -r QuestPatcher/bin/Release/net5.0/osx-x64/publish/* macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/
 echo Building DMG file . . .
 hdiutil create -srcfolder macos-dmg/dmg-root /tmp/QuestPatcher.dmg -volname QuestPatcher
