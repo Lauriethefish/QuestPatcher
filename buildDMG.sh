f3be8cdbbf55da2fc23c9ef8c0b08f8c8fd19b30
Lauriethefish <lauriethefish@outlook.com>
2021-06-06 06:47:10 +0100

    Fix applications symlink properly

--- buildDMG.sh
+++ buildDMG.sh
@@ -1,15 +1,24 @@
-rm -f macos-dmg/QuestPatcher.dmg
+rm -f macos-dmg/QuestPatcher.dmg # Remove old DMG
+
 echo Building with .NET core . . .
 dotnet publish -c Release -r osx-x64
+
+echo Creating applications symlink . . .
+ln -sfn "/Applications" "macos-dmg/dmg-root/Applications"
+
 echo Copying built files to app directory . . .
 mkdir -p macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS
 cp -r QuestPatcher/bin/Release/net5.0/osx-x64/publish/* macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/
+
 echo Building DMG file . . .
 hdiutil info | grep /dev/disk | grep partition | cut -f 1 | xargs hdiutil detach # Fixes attaching error
 hdiutil create -srcfolder macos-dmg/dmg-root /tmp/QuestPatcher.dmg -volname QuestPatcher
+
 echo Removing built files . . .
 rm -r macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/*
+
 echo Compressing DMG file . . .
 hdiutil convert /tmp/QuestPatcher.dmg -format UDZO -o macos-dmg/QuestPatcher.dmg
 rm /tmp/QuestPatcher.dmg
+
 echo Done!
