fe392c24564b27b351a536f73b5727b7adbf3484
Lauriethefish <lauriethefish@outlook.com>
2021-06-06 15:42:26 +0100

    Add LICENSE to mac DMG

--- buildDMG.sh
+++ buildDMG.sh
@@ -10,6 +10,9 @@ echo Copying built files to app directory . . .
 mkdir -p macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS
 cp -r QuestPatcher/bin/Release/net5.0/osx-x64/publish/* macos-dmg/dmg-root/QuestPatcher.app/Contents/MacOS/
 
+echo Adding LICENSE . . .
+cp LICENSE macos-dmg/dmg-root/LICENSE
+
 echo Building DMG file . . .
 hdiutil info | grep /dev/disk | grep partition | cut -f 1 | xargs hdiutil detach # Fixes attaching error
 hdiutil create -srcfolder macos-dmg/dmg-root /tmp/QuestPatcher.dmg -volname QuestPatcher
