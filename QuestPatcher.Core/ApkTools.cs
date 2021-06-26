79306af3255dc45412d840daf19ceafea4f00042
Lauriethefish <lauriethefish@outlook.com>
2021-06-26 12:43:49 +0100

    Sign with debug flag

--- QuestPatcher.Core/ApkTools.cs
+++ QuestPatcher.Core/ApkTools.cs
@@ -73,7 +73,7 @@ namespace QuestPatcher.Core
 
         public Task SignApk(string apkPath)
         {
-            return InvokeJavaTool(ExternalFileType.UberApkSigner, $"--apks \"{apkPath}\"");
+            return InvokeJavaTool(ExternalFileType.UberApkSigner, $"--debug --apks \"{apkPath}\"");
         }
     }
 }
